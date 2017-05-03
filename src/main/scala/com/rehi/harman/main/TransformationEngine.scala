225f793fd9e7965ac57669ff3a1c9132a48fab37
DRehi <DRehi@higurl61740.ad.harman.com>
2017-05-03 11:35:02 +0530

    @Extended NestedJson Support

--- src/main/scala/com/rehi/harman/main/TransformationEngine.scala
+++ src/main/scala/com/rehi/harman/main/TransformationEngine.scala
@@ -5,6 +5,9 @@ import com.rehi.harman.logger.loggerGlobal
 import com.typesafe.scalalogging.slf4j.LazyLogging
 import com.rehi.harman.sparkManager.SparkManager
 
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.hive._
+import org.apache.spark.sql.functions._
 
 object TransformationEngine extends App {
   
@@ -16,10 +19,27 @@ object TransformationEngine extends App {
   val sc = SparkManager.getSparkSession.sparkContext
   
   val sql = SparkManager.getSparkSession.sqlContext
+  
+  import sql.implicits._
 
   val readFile = sql.read.json(sc.wholeTextFiles("C:\\Users\\drehi\\git\\JsonParser\\src\\main\\resources\\test.json", 1).values)
+
+  val readFileDates = readFile.select(explode(readFile("dates")))
+  val readFileRenameToDates = readFile.select(explode(readFile("dates"))).toDF("dates")
+  
+  val readFileColumn = readFile.select(explode(readFile("content")))
+  
+  val readFileColumnRenameToCol = readFile.select(explode(readFile("content"))).toDF("content")
+  
+  val readFileColumnRenameToColField = readFileColumnRenameToCol.select("content.foo","content.bar")
+  
+  
   
   //to show the full content of the column
   readFile.show(false)
-  
+  readFileDates.show(false)
+  readFileRenameToDates.show(false)
+  readFileColumn.show(false)
+  readFileColumnRenameToCol.show(false)
+  readFileColumnRenameToColField.show(false)
 }
